5cc070288437ff8c7601cca0512a9f17ffc5ed01
Jan Jerling <jan.jerling@gmail.com>
2025-01-06 15:46:49 +0200

    Add LookML generation for user tracking events in auto-update script

--- test-js.js
+++ test-js.js
@@ -1,11 +1,36 @@
 const fs = require('fs');
 const path = './my-file.txt';
+const UTE_MAPPING = [
+    ["Login","biometric_login", "login"],
+    ["Login","biometric_login", "impression"],
+    ["Login","biometric_login", "error_impression"],
+    ["Settings","settings", "impression"],
+    ["Settings","notification_settings", "Impression"],
+    ["Settings","security_Settings", "impression"],
+]
 
-// Define your JavaScript function
+// Generate a LookML script to add all mapped user tracking event fields as dimensions and measures
+function makeLookML() {
+    let lookML = [];
+  
+    for (const [header, context, action] of UTE_MAPPING) {
+      let lowContext = context.toLowerCase();
+      let lowAction = action.toLowerCase();
+      const fieldName = `${lowContext}_${lowAction}`.replaceAll(/[.-]/g, "_");
+      
+      var dimension = 'dimension: '+fieldName+' {\n\thidden: yes\n\tgroup_label: "'+header+'"\n\ttype: number\n\tsql: ${TABLE}.'+fieldName+';;\n}\n'
+      var measure = 'measure: total_'+fieldName+' {\n\tlabel: "'+context+':'+action+'"\n\tgroup_label: "'+header+'"\n\ttype: sum\n\tvalue_format: "@{number_format}"\n\tsql: ${'+fieldName+'};;\n}\n'
+      lookML.push(dimension);
+      lookML.push(measure);
+    }
+    
+  return lookML.join('\n');
+}
+
+// Append string to existing file
 function appendDateToFile(filePath) {
   const currentDate = new Date().toISOString();
-  console.log('test');
-
+  fs.appendFileSync(filePath, makeLookML());
 }
 
 // Execute the function
